6b0ac80c6f5c31a7690e73cf44749624eb00a309
AnnaPinkowicz <71516618+AnnaPinkowicz@users.noreply.github.com>
2020-12-07 16:08:04 +0100

    Add files via upload

---
+++ test_tmdb.py
@@ -0,0 +1,26 @@
+def test_get_single_movie(monkeypatch):
+   mock_movie = ['Movie 1']
+   requests_mock = Mock()
+   response = requests_mock.return_value
+   response.json.return_value = mock_movie
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+   movies_list = tmdb_client.get_single_movie()
+   assert movie == mock_movie
+
+def test_get_single_movie_cast(monkeypatch):
+   mock_cast = ["Cast"]
+   requests_mock = Mock()
+   response = requests_mock.return_value
+   response.json.return_value = mock_cast
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+   movie_cast = tmdb_client.get_single_movie_cast()
+   assert movie_cast == mock_cast
+
+def test_get_movie_images(monkeypatch):
+   mock_image = ["Image"]
+   requests_mock = Mock()
+   response = requests_mock.return_value
+   response.json.return_value = mock_cast # tutaj jest zle ale nie wiem jak poprawic
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+   movie_cast = tmdb_client.get_movie_images()
+   assert movie_image == mock_image
