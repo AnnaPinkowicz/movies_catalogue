51a3cc33a2751c65b8928b9125a81355f86d8186
AnnaPinkowicz <71516618+AnnaPinkowicz@users.noreply.github.com>
2020-12-08 19:38:52 +0100

    Add files via upload

--- test_tmdb.py
+++ test_tmdb.py
@@ -1,26 +1,36 @@
+import pytest
+import tmdb_client
+from unittest.mock import Mock
+import requests
+from app import app
+
+def test_get_poster_url_uses_default_size():
+   # Przygotowanie danych
+   poster_api_path = "some-poster-path"
+   expected_default_size = 'w342'
+   # Wywołanie kodu, który testujemy
+   poster_url = tmdb_client.get_poster_url(poster_api_path=poster_api_path)
+   # Porównanie wyników
+   assert expected_default_size in poster_url
+
 def test_get_single_movie(monkeypatch):
-   mock_movie = ['Movie 1']
+   mock_single_movie = 000000
    requests_mock = Mock()
    response = requests_mock.return_value
-   response.json.return_value = mock_movie
+   response.json.return_value = mock_single_movie
    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
-   movies_list = tmdb_client.get_single_movie()
-   assert movie == mock_movie
+
+   single_movie = tmdb_client.get_single_movie(movie_id=602211)
+   assert single_movie == mock_single_movie
+
 
 def test_get_single_movie_cast(monkeypatch):
-   mock_cast = ["Cast"]
+   mock_single_movie_cast = {}
    requests_mock = Mock()
    response = requests_mock.return_value
-   response.json.return_value = mock_cast
+   response.json.return_value = mock_single_movie_cast
    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
-   movie_cast = tmdb_client.get_single_movie_cast()
-   assert movie_cast == mock_cast
 
-def test_get_movie_images(monkeypatch):
-   mock_image = ["Image"]
-   requests_mock = Mock()
-   response = requests_mock.return_value
-   response.json.return_value = mock_image # tutaj jest zle ale nie wiem jak poprawic
-   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
-   movie_cast = tmdb_client.get_movie_images()
-   assert movie_image == mock_image
+   mock_movie_cast = tmdb_client.get_single_movie_cast(movie_id=000000)
+   assert mock_movie_cast == mock_single_movie_cast
+ 
